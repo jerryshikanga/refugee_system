d0e564ef67ea63f8b6ab01c7cbe3ceb37eaf5900
Jerry Shikanga <jerryshikanga@gmail.com>
2018-07-18 18:40:07 +0300

    Switch from mysql to sqlite

--- refugee_system/settings.py
+++ refugee_system/settings.py
@@ -90,14 +90,14 @@ WSGI_APPLICATION = 'refugee_system.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
 
-        'ENGINE': 'django.db.backends.mysql',
-        'HOST': 'localhost',
-        'USER': 'root',
-        'PASSWORD': '',
-        'NAME': 'db_refugee',
+        # 'ENGINE': 'django.db.backends.mysql',
+        # 'HOST': 'localhost',
+        # 'USER': 'root',
+        # 'PASSWORD': '',
+        # 'NAME': 'db_refugee',
     }
 }
 
