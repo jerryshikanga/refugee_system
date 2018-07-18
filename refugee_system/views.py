5b92eeb6e524917d135ded9cf1f65b5a0d93fee2
Jerry Shikanga <jerryshikanga@gmail.com>
2018-07-18 18:33:06 +0300

    Check for zero distributions

--- refugee_system/views.py
+++ refugee_system/views.py
@@ -7,9 +7,15 @@ import math
 
 @login_required
 def index(request):
+    successful_distributions = Distribution.objects.filter(status=True).count()
+    refugee_latest = Refugee.objects.all()[:10]
+    total_distributions = Distribution.objects.all().count()
+    distribution_rate = 0
+    if total_distributions != 0 :
+        distribution_rate = successful_distributions/total_distributions
     context = {
-        "successful_distributions": Distribution.objects.filter(status=True).count(),
-        "refugee_latest": Refugee.objects.all()[:10],
-        "distribution_rate": math.ceil(Distribution.objects.filter(status=True).count()/Distribution.objects.all().count()*100),
+        "successful_distributions": successful_distributions,
+        "refugee_latest": refugee_latest,
+        "distribution_rate": math.ceil(distribution_rate*100),
     }
     return render(request, 'index.html', context)
